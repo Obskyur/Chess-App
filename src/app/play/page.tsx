072bebba86f8e072764f3853132f14c55b6f23f5
Obskyur <parkourman.miller@gmail.com>
2025-03-09 14:37:07 -0700

    fix: fill screen width

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -7,7 +7,7 @@ export default function Play() {
         'pe4', 'pe5', 'nf3', 'nc6', 'bb5', 'na5', 'bxd7'
     ]
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center w-full">
             <div className="flex justify-center items-center w-full h-full">
                 <Image src={chessBoard} className='px-4 py-6 w-full h-full object-contain' alt='chess board' />
             </div>
